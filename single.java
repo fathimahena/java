24411a4c6c3e1cc12dc200e042080184468b4302
fathimahena <150151359+fathimahena@users.noreply.github.com>
2024-04-01 14:13:53 +0530

    Update single.java

---
+++ single.java
@@ -0,0 +1,65 @@
+mca23
+
+import java.util.Scanner;
+
+class Product {
+
+void product (int pcode, String pname, int price) (
+
+System.out.println("pcode:"+pcode);
+
+System.out.println("pname:"+pname);
+
+System.out.println("price:"+price);
+
+int products (int price) (
+
+return price;
+
+}}
+
+class prodmain(
+
+public static void main(String args[]) {
+
+Scanner input-new Scanner(System.in);
+
+System.out.println("enter pcode, pname,price");
+
+Product pl-new Product();
+
+int cl-input.nextInt();
+
+String nl-input.nextLine();
+
+int rl-input.nextInt();
+
+pl.product (cl,nl,rl);
+
+System.out.println("enter pcode,pname,price");
+
+Product o * 2 = neW Product();
+
+int c2-input.nextInt();
+
+String n2-input.nextLine();
+
+int r2-input.nextInt();
+
+p2.product ( c 2,n2) r2);
+
+System.out.println("enter pcode, pname,price");
+
+Product p3-new Product();
+
+int c3-input.nextInt();
+
+String n3-input.nextLine();
+
+int r3-input.nextInt();
+
+p3.product ( c 3,n3,x3) ;
+
+al-pl.products (
+
+}}
